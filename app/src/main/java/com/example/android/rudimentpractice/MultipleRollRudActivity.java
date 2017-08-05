4652b5f6138d0a705fa2a80f8e307a4b9a7bc2c8
encar salazar <caloysalazar2017@gmail.com>
2017-08-05 22:43:55 +0800

    2

---
+++ app/src/main/java/com/example/android/rudimentpractice/MultipleRollRudActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.rudimentpractice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultipleRollRudActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiple_roll_rud);
+    }
+}
